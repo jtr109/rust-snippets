30ff4cee80893059335483ccfaf7200a8fe31019
jtr109 <conbas2019@gmail.com>
2020-11-19 19:05:24 +0800

    multiple files as lib module

--- multiple-lib-module/src/lib.rs
+++ multiple-lib-module/src/lib.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod mod_one; // announce mod_one as a lib module
+
+pub use crate::mod_one::hello;
+
+#[cfg(test)]
+mod test {
+    use super::hello;
+
+    #[test]
+    fn test_say_hello() {
+        let expected = "Hello, Ryan!";
+        assert_eq!(hello("Ryan"), expected);
+    }
 }
